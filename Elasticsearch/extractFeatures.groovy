432887f0d7d4b6dfcffc00139ccfd55d06d381af
manthan787 <manthant15@gmail.com>
2016-08-22 15:53:34 -0400

    Add documentation for groovy script

--- Elasticsearch/extractFeatures.groovy
+++ Elasticsearch/extractFeatures.groovy
@@ -1,7 +1,12 @@
+// Groovy script to fetch n-grams from elasticsearch
+// In order for this script to work, elasticsearch shingles must be setup
+// while creating the index.
+
 def features = []
+int gramSize = 1 // Change this value to get bi-grams or tri-grams
 
 for(String term: doc['text'].values) {
-    if(term.tokenize(" ").size() == 1) {
+    if(term.tokenize(" ").size() <= gramSize) {
       features.add(term);
     }
 };
